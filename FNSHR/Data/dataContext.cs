e75c24b850d1201bc0b01e80b1e5a20fc2884ad6
Akaash Banerjee <plunkels@gmail.com>
2024-02-01 23:22:23 +0800

    update 1st Feb

---
+++ FNSHR/Data/dataContext.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+
+namespace FNSHR.Data
+{
+	public class dataContext : IdentityDbContext
+	{
+		public dataContext(DbContextOptions options) : base(options)
+		{
+
+
+		}
+	}
+}
+
--- FNSHR/Program.cs
+++ FNSHR/Program.cs
@@ -2,12 +2,25 @@ using FNSHR.Data;
 using FNSHR.Services;
 using Microsoft.AspNetCore.Components;
 using Microsoft.AspNetCore.Components.Web;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
 
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddRazorPages();
 builder.Services.AddServerSideBlazor();
+
+var cs = builder.Configuration.GetConnectionString("Default");
+builder.Services.AddDbContext<dataContext>(options => options.UseSqlServer(cs));
+
+builder.Services.AddIdentity<IdentityUser, IdentityRole>(options =>
+{
+    options.Password.RequireDigit = false;
+    options.Password.RequiredLength = 5;
+    options.Password.RequireLowercase = false;
+});
+
 builder.Services.AddSingleton<WeatherForecastService>();
 builder.Services.AddSingleton<AppDbContext>();
 builder.Services.AddSingleton<toDoService>();
